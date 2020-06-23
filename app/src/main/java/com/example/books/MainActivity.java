df53312dd421c3f7df6ca79ebb96667954b6b24e
Santa Claus <bjnzivu@gmail.com>
2020-06-24 01:49:00 +0300

    Api Service connected

--- app/src/main/java/com/example/books/MainActivity.java
+++ app/src/main/java/com/example/books/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.books;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
+import java.io.IOException;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,16 +16,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView tvResult = (TextView) findViewById(R.id.tvResponse);
 
         try {
             URL bookUrl = ApiUtil.buildUrl("cooking");
-            String jsonResult = ApiUtil.getJson(bookUrl);
-            tvResult.setText(jsonResult);
+            new BooksQueryTask().execute(bookUrl);
         }
         catch (Exception e) {
             Log.d("Error", e.getMessage());
         }
+    }
 
+    public class BooksQueryTask extends AsyncTask<URL, Void, String> {
+
+        @Override
+        protected String doInBackground(URL... urls) {
+            URL searchURL = urls[0];
+            String result = null;
+            try {
+                result = ApiUtil.getJson(searchURL);
+            } catch (IOException e) {
+                Log.e("Error", e.getMessage());
+            }
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            TextView tvResult = (TextView) findViewById(R.id.tvResponse);
+            tvResult.setText(result);
+        }
     }
 }
